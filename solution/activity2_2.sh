b81bdbf26adbebc5220c89837cd3baeee0ecf53b
JamesNaan007 <66424950+KSaravanan07@users.noreply.github.com>
2025-03-16 16:03:04 +0530

    Delete solution directory

--- solution/activity2_2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-gnome-terminal -- bash -c "python3 client2.py client1; exec bash"
-gnome-terminal -- bash -c "python3 client2.py client2; exec bash"
-gnome-terminal -- bash -c "python3 client2.py client3; exec bash"
-gnome-terminal -- bash -c "python3 client2.py client4; exec bash"
-gnome-terminal -- bash -c "python3 client2.py client5; exec bash"
-
